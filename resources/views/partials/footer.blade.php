244e249fdd6dd0b1d6a672d2c395f8d0632421c8
shuyshuys <ivlyns@protonmail.com>
2024-08-09 04:48:46 +0700

    update: data on footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,12 +2,14 @@
     <div class="row">
         <div class="col-lg-6">
             <ul class="list-inline mb-0">
-                <li class="list-inline-item"><a href="../dashboard/privacy-policy.html">Privacy Policy</a></li>
+                <li class="list-inline-item"><a href="{{ route('privacy-policy') }}">Privacy Policy</a></li>
                 <li class="list-inline-item"><a href="../dashboard/terms-of-service.html">Terms of Use</a></li>
+                <li class="list-inline-item"><a href="{{ route('faqs') }}">Frequently Asked Questions</a></li>
             </ul>
         </div>
         <div class="col-lg-6 d-flex justify-content-end">
-            Copyright 2020 <a href="#">SocialV</a> All Rights Reserved.
+            Copyright 2024 <a href="#" class="ps-1 pe-2"> Tactick </a> All Rights
+            Reserved.
         </div>
     </div>
 </div>
