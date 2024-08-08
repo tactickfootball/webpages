f301756ed7842cde251cac3f965ae0062016f2ad
shuyshuys <ivlyns@protonmail.com>
2024-08-08 18:27:17 +0700

    add template html file

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -57,7 +57,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-12">
+            <div class="col-sm-6">
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="card">
