fab0785aa1b2572316df934baa9d202b82bc6fb5
shuyshuys <ivlyns@protonmail.com>
2024-08-09 04:23:21 +0700

    make template right sidebar

--- resources/views/partials/right-sidebar-mini.blade.php
+++ resources/views/partials/right-sidebar-mini.blade.php
@@ -8,7 +8,7 @@
                             <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
                         </div>
                         <div class="ms-3 text-center">
-                            <h6>Naruh skor mungkin disini?</h6>
+                            <h6>Naruh skor?</h6>
                             <h6 class="mb-0">1-1</h6>
                         </div>
                         <div class="iq-profile-avatar status-offline">
@@ -16,93 +16,51 @@
                         </div>
                     </div>
                     <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-online">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/01.jpg" alt="">
+                        <div class="iq-profile-avatar status-offline">
+                            <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
                         </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Anna Sthesia</h6>
-                            <p class="mb-0">Just Now</p>
+                        <div class="ms-3 text-center">
+                            <h6>Naruh skor?</h6>
+                            <h6 class="mb-0">1-1</h6>
                         </div>
-                    </div>
-                    <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-online">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/02.jpg" alt="">
-                        </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Paul Molive</h6>
-                            <p class="mb-0">Admin</p>
+                        <div class="iq-profile-avatar status-offline">
+                            <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
                         </div>
                     </div>
                     <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-online">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/03.jpg" alt="">
+                        <div class="iq-profile-avatar status-offline">
+                            <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
                         </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Anna Mull</h6>
-                            <p class="mb-0">Admin</p>
+                        <div class="ms-3 text-center">
+                            <h6>Naruh skor?</h6>
+                            <h6 class="mb-0">1-1</h6>
                         </div>
-                    </div>
-                    <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-online">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/04.jpg" alt="">
-                        </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Paige Turner</h6>
-                            <p class="mb-0">Admin</p>
+                        <div class="iq-profile-avatar status-offline">
+                            <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
                         </div>
                     </div>
                     <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-online">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/11.jpg" alt="">
+                        <div class="iq-profile-avatar status-offline">
+                            <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
                         </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Bob Frapples</h6>
-                            <p class="mb-0">Admin</p>
+                        <div class="ms-3 text-center">
+                            <h6>Naruh skor?</h6>
+                            <h6 class="mb-0">1-1</h6>
                         </div>
-                    </div>
-                    <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-online">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/02.jpg" alt="">
-                        </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Barb Ackue</h6>
-                            <p class="mb-0">Admin</p>
+                        <div class="iq-profile-avatar status-offline">
+                            <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
                         </div>
                     </div>
                     <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-online">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/03.jpg" alt="">
+                        <div class="iq-profile-avatar status-offline">
+                            <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
                         </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Greta Life</h6>
-                            <p class="mb-0">Admin</p>
+                        <div class="ms-3 text-center">
+                            <h6>Naruh skor?</h6>
+                            <h6 class="mb-0">1-1</h6>
                         </div>
-                    </div>
-                    <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-away">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/12.jpg" alt="">
-                        </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Ira Membrit</h6>
-                            <p class="mb-0">Admin</p>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-away">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/01.jpg" alt="">
-                        </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Pete Sariya</h6>
-                            <p class="mb-0">Admin</p>
-                        </div>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <div class="iq-profile-avatar">
-                            <img class="rounded-circle avatar-50" src="../assets/images/user/02.jpg" alt="">
-                        </div>
-                        <div class="ms-3">
-                            <h6 class="mb-0">Monty Carlo</h6>
-                            <p class="mb-0">Admin</p>
+                        <div class="iq-profile-avatar status-offline">
+                            <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
                         </div>
                     </div>
                 </div>
