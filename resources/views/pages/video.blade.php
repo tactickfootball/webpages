99ed42a0700a7b7a0d84a951b8682fc59505c10e
shuyshuys <ivlyns@protonmail.com>
2024-08-12 16:09:08 +0000

    add: video page

---
+++ resources/views/pages/video.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.app')
+
+@section('title', 'Tactick - Videos')
+
+@section('content')
+<div class="container relative">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-body pt-0">
+                            <div class="iq-todo-page">
+                                <div class="add-new-project mt-3 mb-3">
+                                    <h3 href="#" class="d-block nrw-project"><i class="ri-movie-line"></i>
+                                        Videos
+                                    </h3>
+                                </div>
+                                <div class="row no-gutters mb-2">
+                                    <div class="col-md-4">
+                                        <iframe class="embed-responsive-item" width="100%"
+                                            src="https://www.youtube.com/embed/cS2HdOdWycs?si=fUpPpHQLzTwVZR7x"
+                                            title="YouTube video player" frameborder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title <span class="float-end"><i
+                                                        class="ri-bookmark-line"></i></span></h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a
+                                                natural lead-in. a little bit longer.</p>
+                                            <p class="card-text"><small class="text-muted">Last updated 3 mins
+                                                    ago</small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row no-gutters mb-2">
+                                    <div class="col-md-4">
+                                        <iframe class="embed-responsive-item" width="100%"
+                                            src="https://www.youtube.com/embed/cS2HdOdWycs?si=fUpPpHQLzTwVZR7x"
+                                            title="YouTube video player" frameborder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title <span class="float-end"><i
+                                                        class="ri-bookmark-fill"></i></span></h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a
+                                                natural lead-in. a little bit longer.</p>
+                                            <p class="card-text"><small class="text-muted">Last updated 3 mins
+                                                    ago</small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row no-gutters mb-2">
+                                    <div class="col-md-4">
+                                        <iframe class="embed-responsive-item" width="100%"
+                                            src="https://www.youtube.com/embed/cS2HdOdWycs?si=fUpPpHQLzTwVZR7x"
+                                            title="YouTube video player" frameborder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title <span class="float-end"><i
+                                                        class="ri-bookmark-line"></i></span></h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a
+                                                natural lead-in. a little bit longer.</p>
+                                            <p class="card-text"><small class="text-muted">Last updated 3 mins
+                                                    ago</small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
