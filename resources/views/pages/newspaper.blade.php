85cf34a4e23227ce18d2a12f31c7a4830dd9f61e
shuyshuys <ivlyns@protonmail.com>
2024-08-12 16:10:05 +0000

    add: newpaper page

---
+++ resources/views/pages/newspaper.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('title', 'Tactick - Newspaper')
+
+@section('content')
+<div class="container relative">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-body pt-0">
+                            <div class="iq-todo-page">
+                                <div class="add-new-project mt-3 mb-3">
+                                    <h3 href="#" class="d-block nrw-project"><i class="ri-newspaper-line"></i>
+                                        Trending News
+                                    </h3>
+                                </div>
+                                <div class="row no-gutters mb-2">
+                                    <div class="col-md-4">
+                                        <img src="../assets/images/page-img/08.jpg" class="card-img" alt="#">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title <span class="float-end"><i
+                                                        class="ri-bookmark-line"></i></span></h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a
+                                                natural lead-in. a little bit longer.</p>
+                                            <p class="card-text"><small class="text-muted">Last updated 3 mins
+                                                    ago</small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row no-gutters mb-2">
+                                    <div class="col-md-4">
+                                        <img src="../assets/images/page-img/08.jpg" class="card-img" alt="#">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title <span class="float-end"><i
+                                                        class="ri-bookmark-fill"></i></span></h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a
+                                                natural lead-in. a little bit longer.</p>
+                                            <p class="card-text"><small class="text-muted">Last updated 3 mins
+                                                    ago</small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row no-gutters mb-2">
+                                    <div class="col-md-4">
+                                        <img src="../assets/images/page-img/08.jpg" class="card-img" alt="#">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title <span class="float-end"><i
+                                                        class="ri-bookmark-line"></i></span></h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a
+                                                natural lead-in. a little bit longer.</p>
+                                            <p class="card-text"><small class="text-muted">Last updated 3 mins
+                                                    ago</small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
