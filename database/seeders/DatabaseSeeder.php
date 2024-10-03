cf8365847423ba9e281cc6c9edf1465dc004fd92
shuyshuys <ivlyns@protonmail.com>
2024-10-03 14:57:27 +0700

    feat: Add seeders for clubs, matches, mitra, and seats tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            ClubsTableSeeder::class,
+            MitraTableSeeder::class,
+            MatchesTableSeeder::class,
+            SeatsTableSeeder::class,
+        ]);
         // User::factory(10)->create();
 
         // User::factory()->create([
