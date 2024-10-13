afeb00fc2e5285172316e9f0ff4d57a15577314f
shuyshuys <ivlyns@protonmail.com>
2024-10-13 12:31:52 +0700

    style: Update matches-detail page to display match details in Indonesian language

--- resources/views/pages/matches-detail.blade.php
+++ resources/views/pages/matches-detail.blade.php
@@ -1,25 +1,28 @@
 @extends('layouts.app-plain')
 
-@section('title', 'Detail Match - Tactick')
+@section('title', 'Detail Pertandingan - Tactick')
 
 @section('content')
     <div class="container">
         <div class="col-lg-12">
             @include('partials.breadcrumb')
         </div>
-        <h1>Match Details</h1>
-        @foreach ($matches as $match)
-            <p>Date: {{ $match->date }}</p>
-            <p>Time: {{ $match->time }}</p>
-            <p>Teams: <img src="{{ $match->getClub1->logo }}" alt="" height="100px"> VS <img
-                    src="{{ $match->getClub2->logo }}" alt="" height="100px"></p>
-            <p>Name: {{ $match->name }}</p>
-            <p>Description: {{ $match->description }}</p>
-
-            <td class="col-lg-2">
-                {{-- <a href="{{ route('ticket-detail') }}" class="btn btn-primary">Buy</a> --}}
-                <a href="{{ route('ticket-detail', ['id' => $match->id]) }}" class="btn btn-primary">Buy</a>
-            </td>
-        @endforeach
+        <div class="card">
+            <div class="card-body">
+                <h1>Detail Pertandingan</h1>
+                @foreach ($matches as $match)
+                    <p>Nama Pertandingan: {{ $match->name }}</p>
+                    <p>Deskripsi: {{ $match->description }}</p>
+                    <p>Tim: <img src="{{ $match->getClub1->logo }}" alt="" height="100px"> VS <img
+                            src="{{ $match->getClub2->logo }}" alt="" height="100px"></p>
+                    <p>Venue: {{ $match->venue }}</p>
+                    <p>Tanggal: {{ $match->date }}</p>
+                    <p>Waktu: {{ $match->time }}</p>
+                @endforeach
+            </div>
+        </div>
+        <td class="col-lg-2">
+            <a href="{{ route('ticket-detail', ['id' => $match->id]) }}" class="btn btn-primary">Pesan Sekarang</a>
+        </td>
     </div>
 @endsection
