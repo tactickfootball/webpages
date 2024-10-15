1d722b900fbbd0d7ef6f0c354002250a39b83112
shuyshuys <ivlyns@protonmail.com>
2024-10-16 04:03:24 +0700

    Add PaymentController for handling payment related requests

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TokopayCallbackController;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/payment/callback', [TokopayCallbackController::class, 'handle']);
+
+Route::get('/payment/{trx_id}', [PaymentController::class, 'showPayment'])->name('payment');
 
 // Health check route
 Route::get('/health', function () {
