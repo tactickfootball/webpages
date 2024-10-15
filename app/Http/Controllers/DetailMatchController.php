0c118013420f56d23b724b08111fadd0b7b15b12
shuyshuys <ivlyns@protonmail.com>
2024-10-16 04:09:26 +0700

    update: variable name

--- app/Http/Controllers/DetailMatchController.php
+++ app/Http/Controllers/DetailMatchController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Ticket;
 use App\Models\Matches;
 
 class DetailMatchController extends Controller
@@ -11,15 +10,14 @@ class DetailMatchController extends Controller
     public function showDetailMatches($id)
     {
         // Fetch data from the matches table
-        $matches = Ticket::with('getClub1')->get()->where('id', $id);
+        $matches = Matches::with('getClub1')->get()->where('id', $id);
 
         $match = Matches::findOrFail($id);
 
         // Add the breadcrumbs
         $breadcrumbs = [
             ['name' => 'Beranda', 'url' => route('index')],
-            // ['name' => 'Match Detail', 'url' => route('match-detail', ['id' => $match->id])],
-            ['name' => $match->name, 'url' => route('match-detail', ['id' => $match->id])],
+            ['name' => $match->name, 'url' => route('match.detail', ['id' => $match->id])],
         ];
 
         // Pass the data to the view
